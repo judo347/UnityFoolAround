99a071d6b99bdb4c81bbb5f911cab2639338cf43
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2018-11-21 08:28:06 +0100

    menu work

---
+++ 004 - ProjectFragmentedSingularity/Project Fragmented Singularity/Assets/Scripts/My Scripts/MenuHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuHandler : MonoBehaviour {
+
+    public void ExitButtonAction(){
+        Application.Quit();
+    }
+
+    public void StartGameAction(){
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
+	}
+}
